1eeb16489e00195e962730f1ece3f38e2e24284d
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-12 20:40:59 +0200

    ajout JQ + améliorations mineures

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#moonBackground1').mouseover(function(){
+        $('#blockContainer1').css('display', 'block');
+        $('#column1').css('background-color', '#B8B7E2');
+    });
+    $('#moonBackground2').mouseover(function(){
+        $('#blockContainer2').css('display', 'block');
+        $('#column2').css('background-color', '#B8B7E2');
+    });
+    $('#moonBackground3').mouseover(function(){
+        $('#blockContainer3').css('display', 'block');
+        $('#column3').css('background-color', '#B8B7E2');
+    });
+    $('#moonBackground4').mouseover(function(){
+        $('#blockContainer4').css('display', 'block');
+        $('#column4').css('background-color', '#B8B7E2');
+    });
+});
